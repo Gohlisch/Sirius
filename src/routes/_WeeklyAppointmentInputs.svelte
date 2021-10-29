ea024f4b6d9131e274d69d80960d786a9bb3e91d
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-30 00:50:18 +0200

    fix: add propper props and closing tags in WeeklyAppointmentInputs

--- src/routes/_WeeklyAppointmentInputs.svelte
+++ src/routes/_WeeklyAppointmentInputs.svelte
@@ -1,73 +1,78 @@
 <script lang="ts">
+    import type { AppointmentSurvey } from "src/model/appointment_survey";
+    import SlotCheckbox from "./_SlotCheckbox.svelte"
+
+    export let survey: AppointmentSurvey;
+
     const options: Intl.DateTimeFormatOptions = {hour: "2-digit", minute: "2-digit"};
 </script>
 
 {#if survey.slots.some((slot) => slot.start.getDay() === 1)}
     <fieldset>
-        <Legend>Montag</Legend>
+        <legend>Montag</legend>
         {#each survey.slots as slot, i}
             {#if slot.start.getDay() === 1}
-                <SlotCheckbox slot="{slot}" i="{i}">
+                <SlotCheckbox numberedSlot="{{slot: slot, number: i}}"/>
             {/if}
         {/each}
     </fieldset>
 {/if}
-{#if survey.slots.some((slot) => slot.start.getDay() === 1)}
+{#if survey.slots.some((slot) => slot.start.getDay() === 2)}
     <fieldset>
-        <Legend>Dienstag</Legend>
+        <legend>Dienstag</legend>
         {#each survey.slots as slot, i}
             {#if slot.start.getDay() === 2}
-                <SlotCheckbox slot="{slot}" i="{i}">
+                <SlotCheckbox numberedSlot="{{slot: slot, number: i}}"/>
             {/if}
         {/each}
     </fieldset>
 {/if}
-{#if survey.slots.some((slot) => slot.start.getDay() === 1)}
+{#if survey.slots.some((slot) => slot.start.getDay() === 3)}
 <fieldset>
-    <Legend>Mittwoch</Legend>
+    <legend>Mittwoch</legend>
     {#each survey.slots as slot, i}
         {#if slot.start.getDay() === 3}
-            <SlotCheckbox slot="{slot}" i="{i}">
+            <SlotCheckbox numberedSlot="{{slot: slot, number: i}}"/>
         {/if}
     {/each}
 </fieldset>
 {/if}
-{#if survey.slots.some((slot) => slot.start.getDay() === 1)}
+{#if survey.slots.some((slot) => slot.start.getDay() === 4)}
 <fieldset>
-    <Legend>Donnerstag</Legend>
+    <legend>Donnerstag</legend>
     {#each survey.slots as slot, i}
         {#if slot.start.getDay() === 4}
-            <SlotCheckbox slot="{slot}" i="{i}">
+            <SlotCheckbox numberedSlot="{{slot: slot, number: i}}"/>
         {/if}
     {/each}
 </fieldset>
 {/if}
-{#if survey.slots.some((slot) => slot.start.getDay() === 1)}
+{#if survey.slots.some((slot) => slot.start.getDay() === 5)}
 <fieldset>
-    <Legend>Freitag</Legend>
+    <legend>Freitag</legend>
     {#each survey.slots as slot, i}
         {#if slot.start.getDay() === 5}
-            <SlotCheckbox slot="{slot}" i="{i}">
+            <SlotCheckbox numberedSlot="{{slot: slot, number: i}}"/>
         {/if}
     {/each}
 </fieldset>
 {/if}
-{#if survey.slots.some((slot) => slot.start.getDay() === 1)}
+{#if survey.slots.some((slot) => slot.start.getDay() === 6)}
 <fieldset>
-    <Legend>Samstag</Legend>
+    <legend>Samstag</legend>
     {#each survey.slots as slot, i}
         {#if slot.start.getDay() === 6}
-            <SlotCheckbox slot="{slot}" i="{i}">
+            <SlotCheckbox numberedSlot="{{slot: slot, number: i}}"/>
         {/if}
     {/each}
 </fieldset>
 {/if}
-{#if survey.slots.some((slot) => slot.start.getDay() === 1)}
+{#if survey.slots.some((slot) => slot.start.getDay() === 7)}
 <fieldset>
-    <Legend>Sonntag</Legend>
+    <legend>Sonntag</legend>
     {#each survey.slots as slot, i}
         {#if slot.start.getDay() === 7}
-            <SlotCheckbox slot="{slot}" i="{i}">
+            <SlotCheckbox numberedSlot="{{slot: slot, number: i}}"/>
         {/if}
     {/each}
 </fieldset>
