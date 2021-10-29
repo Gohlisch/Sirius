705838d6edfea3f2496f4e3755cb8523a7ae5990
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-29 23:31:22 +0200

    feat: rework __error to fit sveltes style

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,13 +1,31 @@
-<div class="grid_container">
-    <%- include("includes/header.ejs") %>
-    <%- include("includes/nav.ejs") %>
-    <main>
-        <h2>Ups! Etwas ist schiefgelaufen! 🙁</h2>
-        <p>Fehlercode <%- error_code %></p>
-        <p><%- error_message %></p>
-        <% if(error_media) { %>
-        <img src="img/<%= error_media.pathFromPublic %>"
-        alt="<%= error_media.alt %>">
-        <% } %>
-    </main>
-</div>
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				errorInfo:{
+                    title: `${status}: ${error.message}`,
+                    statusCode: status,
+                    message: error.message
+                }
+			}
+		};
+	}
+</script>
+
+<script>
+    export let errorInfo;
+
+    const errorMedia = {
+        "404": {src: "", alt:""}
+    }
+</script>
+<main>
+    <h1>{errorInfo.title}</h1>
+    <p>Ups! Etwas ist schiefgelaufen! 🙁</p>
+    <p>Fehlercode {errorInfo.statusCode}</p>
+    <p>{errorInfo.message}</p>
+    {#if errorInfo.statusCode[""+errorInfo.statusCode] }
+        <img src="{errorMedia[""+errorInfo.statusCode].src}"
+        alt="{errorMedia[""+errorInfo.statusCode].alt}">
+    {/if}
+</main>
