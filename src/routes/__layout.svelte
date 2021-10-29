e25aeafba4db67e8c2dcb519e135409412cb67d7
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-29 23:46:01 +0200

    feat: import app.css in __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,7 @@
     import Nav from "./_Nav.svelte";
     import Header from "./_Header.svelte";
     import Footer from "./_Footer.svelte";
+	import '../app.css';
 </script>
 
 <div class="grid_container">
