bc62a5d0075ebb3dfc204c626ae5670642bbca61
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-29 03:38:15 +0200

    feat: edit DailyAppointmentResultTable to fit sveltes style

--- src/routes/_DailyAppointmentResultTable.svelte
+++ src/routes/_DailyAppointmentResultTable.svelte
@@ -1,4 +1,10 @@
-<% const options = {hour: "2-digit", minute: "2-digit"}; %>
+<script lang="ts">
+    import type { AppointmentSurvey } from "src/model/appointment_survey";
+
+    export let survey: AppointmentSurvey;
+
+    const options: Intl.DateTimeFormatOptions = {hour: "2-digit", minute: "2-digit"};
+</script>
 <thead>
 <tr>
     <th>Uhrzeit</th>
@@ -7,29 +13,30 @@
 </tr>
 </thead>
 <tbody>
-<% for(const slot of survey.slots) {
-if(slot.participants.length > 0) { %>
+    {#each survey.slots as slot}
+        {#if slot.participants.length > 0}
     <tr>
         <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.start.toLocaleTimeString("de", options) }</time>
             bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.end.toLocaleTimeString("de", options) }</time>
         </td>
-        <td><%= slot.participants.length %></td>
+        <td>{ slot.participants.length }</td>
         <td>
-            <% if(slot.participants.length > 0) { %>
-            <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
+            {#if slot.participants.length > 0}
+                {slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`))}
+            {/if}
         </td>
     </tr>
-<% } } %>
-<% if(survey.indisposedParticipants.length > 0) { %>
+        {/if}
+    {/each}
+{#if survey.indisposedParticipants.length > 0}
     <tr>
         <td>verhindert</td>
-        <td><%= survey.indisposedParticipants.length %></td>
-        <% if(survey.indisposedParticipants.length>0) { %>
-        <td><%= survey.indisposedParticipants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %></td>
-        <% } %>
+        <td>{ survey.indisposedParticipants.length }</td>
+        {#if survey.indisposedParticipants.length>0}
+        <td>{ survey.indisposedParticipants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }</td>
+        {/if}
     </tr>
-<% } %>
+{/if}
 </tbody>
