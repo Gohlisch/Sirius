0ef8d0d6eb7cdcc17a7506f2686bc8aa8ad6d60b
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-29 22:40:18 +0200

    feat: rework SlotCheckbox to fit sveltes style

--- src/routes/_SlotCheckbox.svelte
+++ src/routes/_SlotCheckbox.svelte
@@ -1,13 +1,16 @@
-<%
-const options = {hour: "2-digit", minute:"2-digit"};
-/**
- * @type {TimeSlot}
- * @external slot
- */
-%>
+<script lang="ts">
+    import type { TimeSlot } from "src/model/time_slot";
+
+    export let numberedSlot: {
+        slot: TimeSlot,
+        number: number
+    };    
+
+    const options: Intl.DateTimeFormatOptions = {hour: "2-digit", minute:"2-digit"};
+</script>
 
 <label>
-    <time><%= slot.start.toLocaleTimeString("de", options) %></time> bis
-    <time><%= slot.end.toLocaleTimeString("de", options) %></time>
-    <input type="checkbox" name="slot<%= i %>">
+    <time>{ numberedSlot.slot.start.toLocaleTimeString("de", options) }</time> bis
+    <time>{ numberedSlot.slot.end.toLocaleTimeString("de", options) }</time>
+    <input type="checkbox" name="slot{numberedSlot.number}">
 </label>
