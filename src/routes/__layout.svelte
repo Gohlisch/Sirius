116d3f44bccf77d50a34db41e1e8c80a7b28b08d
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-29 23:31:34 +0200

    fix: imports of __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import Nav from "./_Nav.svelte";
     import Header from "./_Header.svelte";
-    import Footer from "./_Header.svelte";
+    import Footer from "./_Footer.svelte";
 </script>
 
 <div class="grid_container">
     <Header/>
     <Nav/>
-<slot></slot>
+    <slot></slot>
     <Footer/>
 </div>
