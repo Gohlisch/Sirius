f6e78dae44600aace8049cfab146ef343ee99fad
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-29 03:29:26 +0200

    feat: edit WeeklyAppointmentResultTable to fit sveltes style

--- src/routes/_WeeklyAppointmentResultTable.svelte
+++ src/routes/_WeeklyAppointmentResultTable.svelte
@@ -1,4 +1,9 @@
-<% const options = {hour: "2-digit", minute: "2-digit"}; %>
+<script lang="ts">
+    import type { AppointmentSurvey } from "src/model/appointment_survey";
+
+    export let survey: AppointmentSurvey;
+    const options: Intl.DateTimeFormatOptions = {hour: "2-digit", minute: "2-digit"};
+</script> 
 <thead>
 <tr>
     <th>Termin</th>
@@ -8,140 +13,154 @@
 </tr>
 </thead>
 <tbody>
-<% if(survey.slots.some((slot) => slot.start.getDay() === 1)){
-        for(const slot of survey.slots) {
-if(slot.start.getDay() === 1) { %>
-    <tr>
-        <td>Montag</td>
-        <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
-            bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
-        </td>
-        <td><%= slot.participants.length %></td>
-        <td>
-            <% if(slot.participants.length > 0) { %>
-                <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
-        </td>
-    </tr>
-<% } } } %>
-<% if(survey.slots.some((slot) => slot.start.getDay() === 2)){
-        for(const slot of survey.slots) {
-if(slot.start.getDay() === 2) { %>
+    {#if survey.slots.some((slot) => slot.start.getDay() === 1)}
+        {#each survey.slots as slot }
+            {#if slot.start.getDay() === 1}
+        <tr>
+            <td>Montag</td>
+            <td>
+                <time>{ slot.start.toLocaleTimeString("de", options) }</time>
+                bis
+                <time>{ slot.end.toLocaleTimeString("de", options) }</time>
+            </td>
+            <td>{ slot.participants.length }</td>
+            <td>
+                {#if slot.participants.length > 0 }
+                    { slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }
+                {/if}
+            </td>
+        </tr>
+            {/if}
+        {/each}
+    {/if}
+{#if survey.slots.some((slot) => slot.start.getDay() === 2)}
+      {#each survey.slots as slot }
+            {#if slot.start.getDay() === 2}
     <tr>
         <td>Dienstag</td>
         <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.start.toLocaleTimeString("de", options) }</time>
             bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.end.toLocaleTimeString("de", options) }</time>
         </td>
-        <td><%= slot.participants.length %></td>
+        <td>{ slot.participants.length }</td>
         <td>
-            <% if(slot.participants.length > 0) { %>
-                <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
+            {#if slot.participants.length > 0}
+                { slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }
+            {/if}
         </td>
     </tr>
-<% } } } %>
+        {/if}
+    {/each}
+{/if}
 
-<% if(survey.slots.some((slot) => slot.start.getDay() === 3)){
-        for(const slot of survey.slots) {
-if(slot.start.getDay() === 3) { %>
+{#if survey.slots.some((slot) => slot.start.getDay() === 3)}
+    {#each survey.slots as slot }
+        {#if slot.start.getDay() === 3}
     <tr>
         <td>Mittwoch</td>
         <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.start.toLocaleTimeString("de", options) }</time>
             bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.end.toLocaleTimeString("de", options) }</time>
         </td>
-        <td><%= slot.participants.length %></td>
+        <td>{ slot.participants.length }</td>
         <td>
-            <% if(slot.participants.length > 0) { %>
-                <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
+            {#if slot.participants.length > 0}
+                { slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }
+            {/if}
         </td>
     </tr>
-<% } } } %>
-<% if(survey.slots.some((slot) => slot.start.getDay() === 4)){
-        for(const slot of survey.slots) {
-if(slot.start.getDay() === 4) { %>
+        {/if}
+    {/each}
+{/if}
+{#if survey.slots.some((slot) => slot.start.getDay() === 4)}
+    {#each survey.slots as slot }
+        {#if slot.start.getDay() === 4}
     <tr>
         <td>Donnerstag</td>
         <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.start.toLocaleTimeString("de", options) }</time>
             bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.end.toLocaleTimeString("de", options) }</time>
         </td>
-        <td><%= slot.participants.length %></td>
+        <td>{ slot.participants.length }</td>
         <td>
-            <% if(slot.participants.length > 0) { %>
-                <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
+            {#if slot.participants.length > 0}
+                { slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }
+            {/if}
         </td>
     </tr>
-<% } } } %>
-<% if(survey.slots.some((slot) => slot.start.getDay() === 5)){
-        for(const slot of survey.slots) {
-if(slot.start.getDay() === 5) { %>
+        {/if}
+    {/each}
+{/if}
+{#if survey.slots.some((slot) => slot.start.getDay() === 5)}
+    {#each survey.slots as slot }
+        {#if slot.start.getDay() === 5}
     <tr>
         <td>Freitag</td>
         <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.start.toLocaleTimeString("de", options) }</time>
             bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.end.toLocaleTimeString("de", options) }</time>
         </td>
-        <td><%= slot.participants.length %></td>
+        <td>{ slot.participants.length }</td>
         <td>
-            <% if(slot.participants.length > 0) { %>
-                <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
+            {#if slot.participants.length > 0}
+                { slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }
+            {/if}
         </td>
     </tr>
-<% } } } %>
-<% if(survey.slots.some((slot) => slot.start.getDay() === 6)){
-        for(const slot of survey.slots) {
-if(slot.start.getDay() === 6) { %>
+        {/if}
+    {/each}
+{/if}
+{#if survey.slots.some((slot) => slot.start.getDay() === 6)}
+    {#each survey.slots as slot }
+        {#if slot.start.getDay() === 6}
     <tr>
         <td>Samstag</td>
         <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.start.toLocaleTimeString("de", options) }</time>
             bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.end.toLocaleTimeString("de", options) }</time>
         </td>
-        <td><%= slot.participants.length %></td>
+        <td>{ slot.participants.length }</td>
         <td>
-            <% if(slot.participants.length > 0) { %>
-                <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
+            {#if slot.participants.length > 0}
+                { slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }
+            {/if}
         </td>
     </tr>
-<% } } } %>
-<% if(survey.slots.some((slot) => slot.start.getDay() === 0)){
-        for(const slot of survey.slots) {
-if(slot.start.getDay() === 0) { %>
+        {/if}
+    {/each}
+{/if}
+{#if survey.slots.some((slot) => slot.start.getDay() === 7)}
+    {#each survey.slots as slot }
+        {#if slot.start.getDay() === 7}
     <tr>
         <td>Sonntag</td>
         <td>
-            <time><%= slot.start.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.start.toLocaleTimeString("de", options) }</time>
             bis
-            <time><%= slot.end.toLocaleTimeString("de", options) %></time>
+            <time>{ slot.end.toLocaleTimeString("de", options) }</time>
         </td>
-        <td><%= slot.participants.length %></td>
+        <td>{ slot.participants.length }</td>
         <td>
-            <% if(slot.participants.length > 0) { %>
-                <%= slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %>
-            <% } %>
+            {#if slot.participants.length > 0}
+                { slot.participants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }
+            {/if}
         </td>
     </tr>
-<% } } } %>
-<% if(survey.indisposedParticipants.length > 0) { %>
+        {/if}
+    {/each}
+{/if}
+{#if survey.indisposedParticipants.length > 0 }
     <tr>
         <td colspan="2">verhindert</td>
-        <td><%= survey.indisposedParticipants.length %></td>
-        <% if(survey.indisposedParticipants.length>0) { %>
-            <td><%= survey.indisposedParticipants.reduce((p1, p2) => p1.concat(`, ${p2}`)) %></td>
-        <% } %>
+        <td>{ survey.indisposedParticipants.length }</td>
+        {#if survey.indisposedParticipants.length>0 }
+            <td>{ survey.indisposedParticipants.reduce((p1, p2) => p1.concat(`, ${p2}`)) }</td>
+        {/if}
     </tr>
-<% } %>
+{/if}
 </tbody>
