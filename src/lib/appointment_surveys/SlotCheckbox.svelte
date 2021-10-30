5005a49f26abe5bf5edb52aec0b9310ad62bbb87
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-30 19:54:01 +0200

    fix: bind:checked of SlotCheckboxes to the inserted PickedSlot value

--- src/lib/appointment_surveys/SlotCheckbox.svelte
+++ src/lib/appointment_surveys/SlotCheckbox.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type { PickedSlot } from "src/model/picked_slot";
-    
+        
     export let pickedSlot: PickedSlot;
 
     const options: Intl.DateTimeFormatOptions = {hour: "2-digit", minute:"2-digit"};
@@ -9,5 +9,5 @@
 <label class:selected_slot={pickedSlot.picked}>
     <time>{ pickedSlot.slot.start.toLocaleTimeString("de", options) }</time> bis
     <time>{ pickedSlot.slot.end.toLocaleTimeString("de", options) }</time>
-    <input type="checkbox" name="slot">
+    <input type="checkbox" name="slot" bind:checked={pickedSlot.picked}>
 </label>
