313682cf6c57374f57ee8fba291eca31620a8be7
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-10-31 19:42:30 +0100

    feat: use transition and change styles on Notification

--- src/lib/notifications/Notification.svelte
+++ src/lib/notifications/Notification.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type { Message } from "./notification_store";
+	import { fly } from 'svelte/transition';
 
     export let message: Message;
 </script>
@@ -8,8 +9,9 @@
     article {
         display: flex;
         align-items: center;
+        font-size: 0.75em;
         padding: 0 0.5em 0 .05em;
-        margin: 0.5em 1em 0.5em 1em;
+        margin: 0.5em 0.75em 0.5em 0.75em;
         border: 1px solid black;
         border-radius: 0.5em;
     }
@@ -31,7 +33,7 @@
     }
 </style>
 
-<article class="{message.type || ""}">
+<article class="{message.type || ""}" in:fly="{{ x: 200, duration: 500 }}" out:fly="{{ y: -100, duration: 750 }}">
     {#if message.iconPath}
     <div><img src="{message.iconPath}" alt="Icon" aria-hidden="true"></div>
     {/if}
