08cbaa8caf77094aa2cfdffd91caff91d41daa4e
clemens.kurzeya <clemens.kurzeya@icloud.com>
2021-11-04 12:58:25 +0100

    started to add service for surveys.

--- backend/src/survey/survey.module.ts
+++ backend/src/survey/survey.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { SurveyController } from './survey.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppointmentSurvey } from '../entities/survey.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AppointmentSurvey])],
   providers: [SurveyService],
   controllers: [SurveyController],
 })
--- backend/src/survey/survey.service.ts
+++ backend/src/survey/survey.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AppointmentSurvey } from '../entities/survey.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SurveyService {}
+export class SurveyService {
+  constructor(
+    @InjectRepository(AppointmentSurvey)
+    private surveyRepository: Repository<AppointmentSurvey>,
+  ) {}
+
+  /**
+   * returns all saved surveys.
+   */
+  async getAll(): Promise<AppointmentSurvey[]> {
+    return this.surveyRepository.find();
+  }
+
+  /**
+   * finds a survey for given id.
+   * @param id of survey to search for
+   */
+  getById(id: string): Promise<AppointmentSurvey> {
+    return this.surveyRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  /**
+   * removes survey by id.
+   * @param id from survey to remove
+   */
+  async removeById(id: string): Promise<AppointmentSurvey> {
+    const appointmentSurvey = await this.getById(id);
+    if (appointmentSurvey) {
+      return this.surveyRepository.remove(appointmentSurvey);
+    }
+  }
+
+  /**
+   * removes the given survey from db.
+   * @param survey
+   */
+  remove(survey: AppointmentSurvey): Promise<AppointmentSurvey> {
+    return this.surveyRepository.remove(survey);
+  }
+}
