9394ce5785151b76b9fe3c1f40886afd9c56b9d0
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-11-04 21:19:41 +0100

    add ui demo and colors

---
+++ src/routes/ui_demo.svelte
@@ -0,0 +1,21 @@
+<script>
+    const colors = ["red", "orange", "yellow", "green", "mint", "teal", "cyan", "blue", "indigo", "purple", "pink", "brown"];
+</script>
+
+<style>
+    .colors {
+        display: flex;
+    }
+
+    .color_block {
+        height: 100px;
+        width: 100px;
+        color: var(--brightest_colors);
+    }
+</style>
+
+<div class="colors">
+    {#each colors as color}
+    <div class="color_block" style="background-color: {color}">{color}</div>
+    {/each}
+</div>
