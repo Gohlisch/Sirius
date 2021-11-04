e36ac8ce8198068fb5fc163ceaf821cef32d6e20
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2021-11-04 21:44:44 +0100

    feat(ui_demo): show purpose colors

--- frontend/src/routes/ui_demo.svelte
+++ frontend/src/routes/ui_demo.svelte
@@ -1,7 +1,7 @@
 <script>
     const colors = ["red", "orange", "yellow", "green", "mint", "teal", "cyan", "blue", "indigo", "purple", "pink", "brown"];
     const shades_of_grey = ["darkest", "darker", "dark", "bright", "brighter", "brightest"]
-    const purpose_color = ["font", "primary", "secondary", "important_signifier_color"];
+    const purpose_color = ["font", "primary", "secondary", "important_signifier"];
 </script>
 
 <style>
@@ -29,6 +29,14 @@
         <h1>6 Shades of Grey™️</h1>
         <div class="colors">
             {#each shades_of_grey as color}
+            <div class="color_block" style="background-color: var(--{color}_color);">{color}</div>
+            {/each}
+        <div/>
+    </article>
+    <article>
+        <h1>Purpose Colors</h1>
+        <div class="colors">
+            {#each purpose_color as color}
             <div class="color_block" style="background-color: var(--{color}_color);">{color}</div>
             {/each}
         <div/>
