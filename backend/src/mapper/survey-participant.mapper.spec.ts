25527e440c157811ec94d67156a9e7cd495d14c5
clemens.kurzeya <clemens.kurzeya@icloud.com>
2021-11-06 23:00:26 +0100

    finished tests for survey-participant mapper.

--- backend/src/mapper/survey-participant.mapper.spec.ts
+++ backend/src/mapper/survey-participant.mapper.spec.ts
@@ -2,26 +2,45 @@ import SurveyParticipantDto from '../model/dtos/survey-participant.dto';
 import { AppointmentSurvey } from '../model/entities/survey.entity';
 import { Repetition } from '../model/enum/repitition.enum';
 import SurveyParticipant from '../model/entities/survey-participant.entity';
-import { mapSurveyParticipantDto } from './survey-participant.mapper';
+import {
+  mapSurveyParticipantDto,
+  mapSurveyParticipantDtos,
+} from './survey-participant.mapper';
 
 describe('survey-participant mapper', () => {
-  let dto: SurveyParticipantDto;
-  let survey: AppointmentSurvey;
-  let date: Date;
-
-  beforeAll(() => {
-    date = new Date();
-    dto = new SurveyParticipantDto('name', date);
-    survey = new AppointmentSurvey('title', Repetition.NEVER, 'des', [], []);
-  });
+  const survey: AppointmentSurvey = new AppointmentSurvey(
+    'title',
+    Repetition.NEVER,
+    'des',
+    [],
+    [],
+  );
+  const date: Date = new Date();
+  const dto: SurveyParticipantDto = new SurveyParticipantDto('name', date);
+  const surveyParticipant: SurveyParticipant = new SurveyParticipant(
+    'name',
+    date,
+    survey,
+  );
 
   it('should map dto to entity', function () {
-    const surveyParticipant: SurveyParticipant = new SurveyParticipant(
-      'name',
-      date,
-      survey,
-    );
     const surveyParticipantMapped = mapSurveyParticipantDto(dto, survey);
     expect(surveyParticipantMapped).toEqual(surveyParticipant);
+  });
+
+  it('should map dtos to entities', function () {
+    const surveyParticipants: SurveyParticipant[] = [];
+    surveyParticipants.push(surveyParticipant);
+    surveyParticipants.push(new SurveyParticipant('name2', date, survey));
+
+    const surveyParticipantDtos: SurveyParticipantDto[] = [];
+    surveyParticipantDtos.push(dto);
+    surveyParticipantDtos.push(new SurveyParticipantDto('name2', date));
+    const surveyParticipantsMapped = mapSurveyParticipantDtos(
+      surveyParticipantDtos,
+      survey,
+    );
+
+    expect(surveyParticipantsMapped).toEqual(surveyParticipants);
   });
 });
